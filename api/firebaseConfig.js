28ecfb6a6b5651a61e4381f59722b93fe564bf30
joaovitorcl <joaovitorcatarinaleite89@gmail.com>
2025-09-16 12:02:59 -0300

    Create firebaseConfig.js

---
+++ api/firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAr0X7FQJ2wieQCnKbhacnSq2avkhBFNS8",
+  authDomain: "teste-momento-bc730.firebaseapp.com",
+  projectId: "teste-momento-bc730",
+  storageBucket: "teste-momento-bc730.firebasestorage.app",
+  messagingSenderId: "939120974666",
+  appId: "1:939120974666:web:bbef6a774aa815ec07bfb3",
+  measurementId: "G-XE8NHLJCZW"
+};
