ef53faa2b371d04099b889de12bbc83ba6b5a597
joaovitorcl <joaovitorcatarinaleite89@gmail.com>
2025-11-20 13:58:40 -0300

    3

--- api/firebaseConfig.js
+++ api/firebaseConfig.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 //inicia o Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 //referencia o banco de dados
 var contactFormDB = firebase.database().ref('teste-momento-bc730');
