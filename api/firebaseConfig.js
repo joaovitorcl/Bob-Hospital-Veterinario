3f3f70ca1412b77dfc42d080f87fa3a9688f4410
joaovitorcl <joaovitorcatarinaleite89@gmail.com>
2025-11-20 13:24:39 -0300

    Update firebaseConfig.js

--- api/firebaseConfig.js
+++ api/firebaseConfig.js
@@ -2,9 +2,16 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAr0X7FQJ2wieQCnKbhacnSq2avkhBFNS8",
   authDomain: "teste-momento-bc730.firebaseapp.com",
+  databaseURL: "https://teste-momento-bc730-default-rtdb.firebaseio.com",
   projectId: "teste-momento-bc730",
   storageBucket: "teste-momento-bc730.firebasestorage.app",
   messagingSenderId: "939120974666",
   appId: "1:939120974666:web:bbef6a774aa815ec07bfb3",
   measurementId: "G-XE8NHLJCZW"
 };
+
+//inicia o Firebase
+firebase.initializeApp(firebaseConfig);
+
+//referencia o banco de dados
+firebase.database().ref('FormularioContato')
